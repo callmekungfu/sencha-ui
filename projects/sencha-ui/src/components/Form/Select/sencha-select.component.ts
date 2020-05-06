70eae28378c363b11c0141b995ff88270b8f1a8b
Yong Lin <wangyonglin1999@gmail.com>
2020-05-06 19:02:39 -0400

    feat(select): added base files for select component

---
+++ projects/sencha-ui/src/components/Form/Select/sencha-select.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sc-select',
+  templateUrl: 'sencha-select.component.html',
+})
+export class SenchaSelectComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ projects/sencha-ui/src/components/Form/Select/sencha-select.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { SenchaSelectComponent } from './sencha-select.component';
+
+@NgModule({
+  imports: [],
+  exports: [SenchaSelectComponent],
+  declarations: [SenchaSelectComponent],
+  providers: [],
+})
+export class SenchaSelectModule {}
